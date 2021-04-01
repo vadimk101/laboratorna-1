8277c0924e13d9ecb6378c9017e5da11755406df
olexandrPeduraruK101 <78420095+olexandrPeduraruK101@users.noreply.github.com>
2021-04-02 01:23:00 +0300

    git commit

---
+++ Labor 1/Labor 1/main.c
@@ -0,0 +1,26 @@
+#include <locale.h>
+#include <stdio.h>
+#include <math.h>//підключення бібліотек
+
+int main() {
+	float r; float h; float S; float V;//ведення зміних
+	float p;
+
+	setlocale(LC_CTYPE, "ru");//міняємо мову
+	printf("Вед1ть рад1ус r\n");//виведення данних на екран
+	scanf("%f",&r); //ведення
+	printf("Вед1ть рад1ус h\n");//виведення данних на екран
+	scanf("%f",&h);//ведення
+
+	p = 3.14159;//число пи
+
+	S = 2 * p * r * h;
+	V = p * pow(r, 2) * h;
+
+	printf("\t S\t V\n");//виведення данних на екран
+	printf("\t %.2f\t %.2f\n", S, V);//виведення данних на екран
+
+
+	return 0;
+}
+
